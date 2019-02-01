166b2ea3c554c9e5e6302be1c9f2fc7fc88b2de4
Wataru Manji <manji@linux.com>
2019-02-02 00:19:30 +0900

    add generator

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+#define y0 y3487465
+#define y1 y8687969
+#define j0 j1347829
+#define j1 j234892
+#define next asdnext
+#define prev asdprev
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define all(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define fill(x,y) memset(x,y,sizeof(x))
+
+typedef long long lint;
+
+int main(){
+    cout << "Hello" << endl;
+
+}
+
